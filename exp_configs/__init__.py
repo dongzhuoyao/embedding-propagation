e8633d2dd881948c3170ce3e0d81257e3fec3d2c
Vincent <taohu620@gmail.com>
2020-09-29 22:12:23 +0200

    u

--- exp_configs/__init__.py
+++ exp_configs/__init__.py
@@ -1,11 +1,15 @@
 from . import pretrain_exps, ssl_exps
 from . import pretrain_exps
+from . import pretrain_miniin_wrn_exps
 from . import finetune_exps
+from . import finetune_fast_exps
 
 EXP_GROUPS = {}
 EXP_GROUPS = pretrain_exps.EXP_GROUPS
 EXP_GROUPS.update(ssl_exps.EXP_GROUPS)
+EXP_GROUPS.update(pretrain_miniin_wrn_exps.EXP_GROUPS)
 EXP_GROUPS.update(finetune_exps.EXP_GROUPS)
+EXP_GROUPS.update(finetune_fast_exps.EXP_GROUPS)
 
 
 
